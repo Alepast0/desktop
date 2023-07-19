98ec4403a3e58a15a6ceaa6f66ad94b4f04c278e
Alexander <sahkushnerev@gmail.com>
2023-07-19 16:57:14 +0300

    new

--- lib/responsive/modile.dart
+++ lib/responsive/modile.dart
@@ -18,24 +18,66 @@ class MobileBody extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(onPressed: () {
-                imageProvider.updateImage('assets/images/Kot.jpg');
-              }, child: const Text("Button 1")),
+              child: ElevatedButton(
+                onPressed: () {
+                  imageProvider.updateImage('assets/images/Kot.jpg');
+                },
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * 0.02,
+                      horizontal: MediaQuery.of(context).size.width * 0.04,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "Button 1",
+                  style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.04),
+                ),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(onPressed: () {
-                imageProvider.updateImage('assets/images/Capybara.jpg');
-              }, child: const Text("Button 2")),
+              child: ElevatedButton(
+                onPressed: () {
+                  imageProvider.updateImage('assets/images/Capybara.jpg');
+                },
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * 0.02,
+                      horizontal: MediaQuery.of(context).size.width * 0.04,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "Button 2",
+                  style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.04),
+                ),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(onPressed: () {
-                imageProvider.updateImage('assets/images/Tiger.jpg');
-              }, child: const Text("Button 3")),
+              child: ElevatedButton(
+                onPressed: () {
+                  imageProvider.updateImage('assets/images/Tiger.jpg');
+                },
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * 0.02,
+                      horizontal: MediaQuery.of(context).size.width * 0.04,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "Button 3",
+                  style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.04),
+                ),
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.only(right: 8, left: 8, bottom: 8 ),
+              padding: const EdgeInsets.only(right: 8, left: 8, bottom: 8),
               child: Image.asset(imageProvider.image),
             )
           ],
